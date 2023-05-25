c0b93c39d812c0c69cd7236fa74476f23337b3e4
xCalonso <96618051+xCalonso@users.noreply.github.com>
2023-05-25 18:56:29 +0200

    nada

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//import getQuotes from './webscraping.js';
-
-//const juegos = getQuotes("Minecraft")
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
