fd1eee32085507375527ef691be604d42f43635b
xCalonso <96618051+xCalonso@users.noreply.github.com>
2023-05-27 11:27:38 +0200

    VUE

--- webscrap.js
+++ webscrap.js
@@ -4,6 +4,66 @@
 const SteamAPI = require('steamapi');
 const steam = new SteamAPI('2A263491854331441324FA092F40370E');
 const stringSimilarity = require('string-similarity');
+const puppeteer = require('puppeteer')
+
+const getQuotes = async (nombre) => {
+  // Start a Puppeteer session with:
+  // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
+  // - no default viewport (`defaultViewport: null` - website page will in full width and height)
+  const browser = await puppeteer.launch({
+    headless: "new",
+    //defaultViewport: null,
+    args:[
+      '--no-sandbox'
+    ]
+  });
+  console.log('hola2')
+
+  // Open a new page
+  const page = await browser.newPage();
+
+  // On this new page:
+  // - open the "http://quotes.toscrape.com/" website
+  // - wait until the dom content is loaded (HTML is ready)
+  await page.goto("https://www.g2a.com/es/", {
+    waitUntil: "domcontentloaded",
+  });
+
+  // Obtiene el HTML de la pagina actual
+  const source = await page.content({"waitUntil": "domcontentloaded"});
+
+  await page.waitForSelector('.indexes__InputContainer-sc-1n30rfz-154 input', {visible: true});
+  await page.type('.indexes__InputContainer-sc-1n30rfz-154 input', nombre);
+  await Promise.all([
+    page.waitForNavigation({waitUntil: "domcontentloaded"}),
+    page.keyboard.press("Enter"),
+  ]);
+
+  // Get page data
+  const scraping = await page.evaluate(() => {
+    // Fetch the first element with class "quote"
+    const divs = document.querySelectorAll(".sc-csTbgd.kglWtV");
+    
+    return Array.from(divs).map((d) => {
+        const div_juego = d.querySelector(".sc-dFRpbK.jtOUCg");
+        const div_precio = d.querySelector(".sc-hBMUJo.bRGwob");
+        
+        const nombre = div_juego.querySelector("a").innerText;
+        const url = div_juego.querySelector("a").href;
+        const precio_act = div_precio.querySelector("span").innerText;
+        
+        return {nombre, url, precio_act};
+    })
+
+  });
+
+  // Display the source
+  //console.log(scraping);
+
+  // Close the browser
+  await browser.close();
+  return scraping;
+};
 
 async function obtenerInfoJuego(nombreJuego) {
   try {
@@ -40,6 +100,7 @@ async function obtenerInfoJuego(nombreJuego) {
     //console.log(nombre);
    
     const urlImagen = game.header_image;
+    const urlStore = game.url;
     return { nombre, precio, urlImagen };
 //catch (error) {
   //  console.error('Error al obtener la información del juego:', error);
